c3daad97a287acbc5df28874d13acd8651446d2a
a-hamza-r <18100216@lums.edu.pk>
2019-02-02 21:29:19 -0500

    Add test cases for new weapons in RoleGame.java

--- src/edu/fsu/cs/cen5035/RoleGame.java
+++ src/edu/fsu/cs/cen5035/RoleGame.java
@@ -15,10 +15,28 @@ public class RoleGame {
         System.out.println("Swords was able to do " + weapon.hit(armor) + " of damage due to an armor with "+armor+ " points.");
 
         weapon = WeaponFactory.getWeapon("spear");
-        System.out.println("Spear has " + weapon.hit() + " of damage.");        
+        System.out.println("Spear has "	 + weapon.hit() + " of damage.");        
         System.out.println("Spear was able to do " + weapon.hit(armor) + " of damage due to an armor with "+armor+ " points.");
         
         //TODO: Add the remainig weapons here
+	
+	// SimpleHammer
+	weapon = WeaponFactory.getWeapon("hammer");
+	System.out.println("SimpleHammer has "  + weapon.hit() + " of damage.");
 
-    }
+        System.out.println("SimpleHammer was able to do " + weapon.hit(armor) + " of damage due to an armor with "+armor+ " points.");
+    
+
+	// CrazyRandomSword
+	weapon = WeaponFactory.getWeapon("crazysword");
+        System.out.println("CrazyRandomSword has "  + weapon.hit() + " of damage.");
+
+        System.out.println("CrazyRandomSword was able to do " + weapon.hit(armor) + " of damage due to an armor with "+armor+ " points.");
+    
+	// MyWeapon
+        weapon = WeaponFactory.getWeapon("myweapon");
+        System.out.println("MyWeapon has "  + weapon.hit() + " of damage.");
+
+        System.out.println("MyWeapon was able to do " + weapon.hit(armor) + " of damage due to an armor with "+armor+ " points.");
+	}
 }
